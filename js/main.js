181e43ae43d24c46a65feb8671be8f3172d888e5
alexgargola1-sudo <alexgargola1@gmail.com>
2026-06-17 15:19:22 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -9,3 +9,58 @@ header.style.background = '#05101d';
 }
 
 });
+const counters = document.querySelectorAll('.counter');
+
+const animateCounters = () => {
+
+    counters.forEach(counter => {
+
+        const target = +counter.getAttribute('data-target');
+
+        let count = 0;
+
+        const increment = target / 100;
+
+        const updateCounter = () => {
+
+            if(count < target){
+
+                count += increment;
+
+                counter.innerText = Math.ceil(count);
+
+                requestAnimationFrame(updateCounter);
+
+            }else{
+
+                counter.innerText = target + '+';
+
+            }
+
+        };
+
+        updateCounter();
+
+    });
+
+};
+
+const statsSection = document.querySelector('.stats');
+
+const observer = new IntersectionObserver(entries => {
+
+    entries.forEach(entry => {
+
+        if(entry.isIntersecting){
+
+            animateCounters();
+
+            observer.disconnect();
+
+        }
+
+    });
+
+});
+
+observer.observe(statsSection);
